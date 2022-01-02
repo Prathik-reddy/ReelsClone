6307bbf187e165f692a4414129f209e1a73a94e1
Prathik Reddy <tvkp333555@gmail.com>
2022-01-02 19:13:08 +0530

    added protected page to avoid non logedIn users to access the protected page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {AuthProvider} from "./Context/AuthContext";
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 import Feed from './components/Feed';
+import PrivateRoute from './components/PrivateRoute';
 function App() {
   return (
     <Router>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Feed/>} />
           <Route path="/Signup" element={<SignUp/>} />
           <Route path="/Login" element={<Login/>} />
+          <Route exact path="/" element={<PrivateRoute />}>
+            <Route exact path="/" element={<Feed />} />
+          </Route>
         </Routes>
       </AuthProvider>
     </Router>
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,12 @@
+import React,{useContext} from 'react';
+import { Navigate,Outlet } from 'react-router';
+import { AuthContext } from '../Context/AuthContext';
+const PrivateRoute = () => {
+    const {user} = useContext(AuthContext);
+    console.log(user);
+    return (
+        user ? <Outlet /> : <Navigate to="/Login" />
+    )
+}
+
+export default PrivateRoute;
