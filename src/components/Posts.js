a98f23e8100fad56d7fd60c975e00e812f7b5590
Prathik Reddy <tvkp333555@gmail.com>
2022-01-08 12:58:43 +0530

    improvement in video section

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,6 +3,8 @@ import {database} from '../firebase';
 import CircularProgress from '@mui/material/CircularProgress';
 import Videos from './Videos'
 import  './Posts.css';
+import Avatar from '@mui/material/Avatar';
+
 const Posts = ({userData}) => {
     console.log("userp",userData);
     const [posts,setPosts] = useState(null);
@@ -30,6 +32,10 @@ const Posts = ({userData}) => {
                                     <React.Fragment key={index}>
                                         <div className="videos">
                                             <Videos src={post.pUrl} id={post.pId}/>
+                                            <div className="fa" style = {{display: 'flex'}}>
+                                                <Avatar sx={{ width: 50, height: 50 }} alt="User Avatar" src={post.profileUrl} />
+                                                <h4>{post.uName}</h4>
+                                            </div>
                                         </div>
                                     </React.Fragment>
                             ))
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import  './Video.css';
+import ReactDOM from 'react-dom';
 
 const Videos = (props) => {
     console.log(props);
+    const handleClick = (e)=>{
+        e.preventDefault();
+        e.target.muted = !e.target.muted;
+    }
+    const handleScroll = (e)=>{
+        e.preventDefault();
+        let nextVideo = ReactDOM.findDOMNode(e.target).parentNode.nextSibling;
+        if(nextVideo){
+            nextVideo.scrollIntoView();
+            e.target.muted = true;
+        }
+    }
     return (
-           <video src ={props.src} id={props.id}  className="videos-styling">
+           <video src ={props.src} id={props.id}  className="videos-styling"  muted = "muted" onClick = {handleClick}  onEnded = {handleScroll}>
 
            </video>
     )
